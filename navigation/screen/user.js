9e8df0dd1ec239388991112c6c72be84c17390a0
ngoduc <ngoduc202@gmail.com>
2023-12-28 20:53:48 +0700

    huong dan github

--- navigation/screen/user.js
+++ navigation/screen/user.js
@@ -1,1 +1,8 @@
 console.log("hello dev Duc");
+//git add . : Cập nhật hết tất cả các file
+//git add -A : cập nhật những file có thay đổi
+
+//git commit -m"tieu de day code" : tieu de day code
+
+//git push
+
